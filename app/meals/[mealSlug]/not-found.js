81078711f6f45ae59232da5d83644853b18d015d
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2025-09-11 05:20:20 +0100

    not-found.js

--- app/meals/[mealSlug]/not-found.js
+++ app/meals/[mealSlug]/not-found.js
@@ -1,6 +1,6 @@
 export default function NotFoundPage(){
 
-  return<main className=" my-40">
+  return<main className=" my-60">
           <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 via-red-600 to-orange-600 font-extrabold  text-xl md:text-2xl xl:text-3xl text-center">Page Not Found...</h1>
           <p className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 via-red-600 to-orange-600 font-bold m-10 text-center text-sm md:text-lg xl:text-3xl">Unfortunately the page you tried to locate doesn&apos;t exist!</p>
         </main>
