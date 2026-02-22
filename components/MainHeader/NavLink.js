19eae3824b84b7e6226c96c52972787b1f63c279
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2026-02-22 21:57:25 +0100

    NavLink.js

--- components/MainHeader/NavLink.js
+++ components/MainHeader/NavLink.js
@@ -6,7 +6,7 @@ export default function NavLink({href, children}){
   const path = usePathname()
 
   return<Link href={href}>
-          <nav className={`cursor-pointer  ${ path.startsWith(href) ? 'text-transparent bg-gradient-to-r from-yellow-300 via-red-600 to-orange-400 bg-clip-text transition-all duration-500' : 'text-white'} drop-shadow-lg hover:drop-shadow-[0_0_10px_rgba(236,92,153,20)]  translate-0.5 scale-110 text-xs md:text-sm xl:text-lg font-bold mx-4 md:m-4 mb-2`}>
+          <nav className={`cursor-pointer  ${ path.startsWith(href) ? 'text-transparent bg-gradient-to-r from-yellow-300 via-red-600 to-orange-400 bg-clip-text transition-all duration-500' : 'text-white'} drop-shadow-lg hover:drop-shadow-[0_0_10px_rgba(236,92,153,20)]  translate-transform scale-105 text-xs md:text-sm xl:text-lg font-bold px-4 py-1 md:m-4 mb-2`}>
             {children}
           </nav>
         </Link>
