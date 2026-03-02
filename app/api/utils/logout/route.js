d8bf48f1dcb6bb4b5d9f58d761c3b81ce38c8d27
Atanda Timothy <timothyabidemi4618@gmail.com>
2026-03-02 16:53:59 +0100

    prisma updated

--- app/api/utils/logout/route.js
+++ app/api/utils/logout/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { hashRefreshToken } from '../tokens/route'
+import prisma from "@/lib/prisma";
 
 export async function POST() {
   const cookieStored = cookies()
